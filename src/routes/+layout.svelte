047d3b4fcca5909d295293738cdae7c8d85f660e
SySLink <shadow.dark2232@gmail.com>
2026-01-01 17:10:02 +0300

    Added Analytics For Vercel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
-	import '../app.css';
-	
+	import "../app.css";
+	import { injectAnalytics } from "@vercel/analytics/sveltekit";
+	import { dev } from "$app/environment";
+
+	injectAnalytics({ mode: dev ? "development" : "production" });
+
 	let { children } = $props();
 </script>
 
